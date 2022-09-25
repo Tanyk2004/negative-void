1943f7f34c00b7df7db094f913fcb7d2c6cf3009
EnyCo <enay@netventix.com>
2022-09-25 16:43:59 -0500

    Update mobController.cs

--- negativeSpace-void/Assets/Scripts/Character/mobController.cs
+++ negativeSpace-void/Assets/Scripts/Character/mobController.cs
@@ -5,7 +5,10 @@ using UnityEngine;
 public class mobController : MonoBehaviour
 {
     public Transform characterTransform;
+    public GameObject player;
+
     [Header("Mob Settings")]
+    public int damage = 10;
     public int characterHealth = 100;
     public Rigidbody2D characterRigidbody;
     // Start is called before the first frame update
@@ -17,8 +20,24 @@ public class mobController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //program the bot here
+        
+	  //program the bot here
     }
+
+    void FixedUpdate()
+    {
+        if (player.transform.position.x > gameObject.transform.position.x){
+		characterTransform.Translate(Time.deltaTime, 0f, 0f);
+	  }
+	  if (player.transform.position.x < gameObject.transform.position.x){
+		characterTransform.Translate(-1*Time.deltaTime, 0f, 0f);
+	  }
+	  if (Mathf.Abs(player.transform.position.x - gameObject.transform.position.x) < 1.5){
+		player.GetComponent<CharacterAttributes>().takeDamage(damage);
+	  }
+	  //program the bot here
+    }
+
     public void takeDamage(int damage)
     {
         Debug.Log("Mob took damage" + damage);
